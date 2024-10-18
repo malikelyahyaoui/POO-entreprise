2ed5f72a69c305a15d8bcc6764ca8fa569d21335
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-18 16:40:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ spl_autoload_register(function ($class_name) {
     require $class_name . '.php';
 });
 
-$elanFormation = new Entreprise("ELAN FORMATION","1993-01-01","14 rue du Rhône","67100","STRASBOURG");
-echo $elanFormation->getRaisonSociale()." a été crée le ".$elanFormation->getDatecreation()->format
-("d.m.Y")." et se situe à l'adresse suivante ".$elanFormation->getAdresseComplete();
+$elanFormation = new Entreprise("ELAN FORMATION","1993-01-01","14 rue du Rhône","67100",
+"STRASBOURG");
+echo $elanFormation;
+
+//echo $elanFormation->getRaisonSociale()." a été crée le ".$elanFormation->getDatecreation()->format
+//("d.m.Y")." et se situe à l'adresse suivante ".$elanFormation->getAdresseComplete();
