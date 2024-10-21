8ea00788c0ce05fc2b2a3a4bfe62598171e08298
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-21 09:44:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,9 @@ spl_autoload_register(function ($class_name) {
 $elanFormation = new Entreprise("ELAN FORMATION","1993-01-01","14 rue du Rhône","67100",
 "STRASBOURG");
 
+$stephane = new Employe("SMAIL","Stéphane","stephane@elan-formation.fr");
 
 
-echo $elanFormation;
+
 echo $elanFormation->getInfos();
+echo $stephane;
