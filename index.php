8153971cac2dfa25f1b619a46da688d6fb6f1b33
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-21 11:01:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,18 @@ spl_autoload_register(function ($class_name) {
 
 $elanFormation = new Entreprise("ELAN FORMATION","1993-01-01","14 rue du Rhône","67100",
 "STRASBOURG");
+$tf1 = new Entreprise("TF1","1970-01-01","13 rue de la Seine","75000",
+"PARIS");
 
-$stephane = new Employe("SMAIL","Stéphane","stephane@elan-formation.fr");
+$stephane = new Employe("SMAIL","Stéphane","stephane@elan-formation.fr",$elanFormation);
+$mickael = new Employe("MURMANN","Mickael","Mickael@elan-formation.fr",$elanFormation);
 
 
 
-echo $elanFormation->getInfos();
-echo $stephane;
+
+//echo $stephane->getInfos();
+//$stephane->setEntreprise($tf1);
+//echo $stephane->getInfos();
+//var_dump($elanFormation);
+
+echo $elanFormation->afficherEmployes();
